e93ead2b377f976b44e7b5761666be6dd3b45057
Igor Delgado <igor.delgado@5labs.com.br>
2022-09-18 23:18:10 -0300

    feature/Planning the StylerEngine

--- src/Style/MediaQueriesController.ts
+++ src/Style/MediaQueriesController.ts
@@ -1,6 +1,7 @@
 import { ComponentFrame } from "../Core/ComponentFrame";
 
 export class MediaQueriesController {
+  protected static $engine: MediaQueriesController;
   static device__width = window.innerWidth;
   static device__height = window.innerWidth;
   static currentBreakPoint: keyof TBreakPoints | null = null;
@@ -13,34 +14,46 @@ export class MediaQueriesController {
     xxl: 1400,
   };
 
+  static eventsOnResize: any[] = []
+
+  static eventWindow = (e: keyof WindowEventMap) =>
+    window.addEventListener(e, () => MediaQueriesController.eventsOnResize.forEach((F) => F()) , false);
+
   constructor() {
-    this.UPDATE_DEVICE_MEASUREMENTS();
-    MediaQueriesController.currentBreakPoint = this.getCurrentBreakPoint();
+    MediaQueriesController.currentBreakPoint =
+      MediaQueriesController.getCurrentBreakPoint();
   }
 
-  private UPDATE_DEVICE_MEASUREMENTS() {
-    return window.addEventListener("resize", () => {
-      MediaQueriesController.device__height = window.innerHeight;
-      MediaQueriesController.device__width = window.innerWidth;
+  get $engine() {
+    if (!MediaQueriesController.$engine) {
+      MediaQueriesController.$engine = new MediaQueriesController();
+      return MediaQueriesController.$engine;
+    }
+    return MediaQueriesController.$engine;
+  }
+
+  protected static ApplyInBreakPoint(
+    breakPoint: keyof TBreakPoints,
+    call: () => ComponentFrame | undefined
+  ) {
+    function handleChangeBreakPoints() {
+      let timer: any = null;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        if (breakPoint === MediaQueriesController.getCurrentBreakPoint()) {
+          call()?.stylize('transition')('0.6s');
+        }
+      }, 200);
+    }
+
+    MediaQueriesController.eventsOnResize.push(handleChangeBreakPoints);
+
+    ["resize", "load"].map((e: any) => {
+      MediaQueriesController.eventWindow(e);
     });
   }
 
-  protected ApplyInBreakPoint(breakPoint: keyof TBreakPoints, call: () => ComponentFrame | undefined) {
-    let timer: any = null;
-    ['resize', 'onload'].map((e) => {
-      (() =>
-        window.addEventListener(e, () => {
-          clearTimeout(timer);
-          timer = setTimeout(() => {
-            if (breakPoint === this.getCurrentBreakPoint()) {
-              call()
-            }
-          }, 200);
-        }))();
-    })
-  }
-
-  private getCurrentBreakPoint() {
+  private static getCurrentBreakPoint() {
     const $breakPointsTest: Array<(w: number) => keyof TBreakPoints | null> = [
       (w) => (w < 576 ? "xs" : null),
 
@@ -60,30 +73,32 @@ export class MediaQueriesController {
     return current;
   }
 
-   xs(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('xs', call)
-    return call()
+  xs(call: () => ComponentFrame | undefined) {
+    MediaQueriesController.ApplyInBreakPoint("xs", call);
+    return call();
   }
   sm(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('sm', call)
-    return call()
+    MediaQueriesController.ApplyInBreakPoint("sm", call);
+    return call();
   }
   md(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('md', call)
-    return call()
+    MediaQueriesController.ApplyInBreakPoint("md", call);
+    return call();
   }
   lg(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('lg', call)
-    return call()
+    MediaQueriesController.ApplyInBreakPoint("lg", call);
+    return call();
   }
   xl(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('xl', call)
-    return call()
+    MediaQueriesController.ApplyInBreakPoint("xl", call);
+    return call();
   }
   xxl(call: () => ComponentFrame | undefined) {
-    this.ApplyInBreakPoint('xxl', call)
-    return call()
+    MediaQueriesController.ApplyInBreakPoint("xxl", call);
+    return call();
   }
 }
 
-export default new MediaQueriesController();
+const mediaQueriesEngine = new MediaQueriesController();
+
+export default mediaQueriesEngine;
--- src/gateway.ts
+++ src/gateway.ts
@@ -10,6 +10,7 @@ Virtual__DOOM.gateway!.style.backgroundColor = ColorPallete.colors.background;
 StyleController.stylize("display", Virtual__DOOM.gateway)("flex");
 StyleController.stylize("justifyContent", Virtual__DOOM.gateway)("center");
 StyleController.stylize("alignItems", Virtual__DOOM.gateway)("center");
+StyleController.stylize("overflow", Virtual__DOOM.gateway)("hidden");
 
 const comp_factory = new ComponentFactory(StyleController);
 
@@ -22,29 +23,53 @@ logo?.xs(() =>
 
 const dots_square_container = comp_factory.make("div", "dots-square-container");
 
-const dots_dark = new SquareDots(comp_factory, "square-dots-container-dark");
-const dots_light = new SquareDots(comp_factory, "square-dots-container-light");
+const dots_dark = new SquareDots(comp_factory, "square-dots-container-dark").render("dk-grey");
+const dots_light = new SquareDots(comp_factory, "square-dots-container-light").render("lh-grey")!;
+
+
 
 Virtual__DOOM.gateway?.insertAdjacentElement("beforeend", Container.element!);
 
-Container.display("flex").justify("center").align("center");
+Container.display("flex").justify("center").align("center").stylize('overflow')('hidden');
 
-dots_square_container
-  .hug(
-    dots_dark.render("lh-grey"),
-    dots_light
-      .render("dk-grey")!
-      .stylize("position")("absolute")
-      .stylize("top")("152px")
-  )
-  .stylize("position")("absolute")
-  .stylize("right")("152px")
-  .xs(() =>
-    dots_square_container
-    .stylize("transform")("scale(0.4)")
-    .stylize("top")("40vh")
-    .stylize("right")("0px")
-  );
+await new Promise (r => {
+  dots_square_container
+    .hug(
+      dots_dark,
+      dots_light
+        .stylize("position")("absolute")
+        .stylize("top")("0px")
+    )
+    .stylize("position")("absolute")
+    // .stylize("right")("152px")
+    .xs(() =>
+      dots_square_container
+        .stylize("transform")("scale(0.4)")
+        .stylize("top")("48vh")
+        // .stylize("right")("0px")
+    )
+    ?.sm(() => dots_square_container.stylize("right")("16vw").stylize("top")("40vh"))
+    ?.md(() => dots_square_container.stylize("right")("50vw"))
+    ?.lg(() => dots_square_container.stylize("right")("50vw"));
+    r(true)
+})
+
+var id: any = null;
+function myMove() {
+  var elem = dots_light?.element   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 152) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem!.style.top = pos + 'px'; 
+    }
+  }
+}
+myMove()
 
 Container.hug(logo, dots_square_container);
 
